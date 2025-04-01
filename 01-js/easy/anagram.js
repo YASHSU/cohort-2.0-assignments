18e1b07b5693e88f59b288528dc9b681f91e93c2
Suyash Chandrakar <50506797+YASHSU@users.noreply.github.com>
2025-04-01 22:42:10 +0530

    Update anagram.js
    Assignment Solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+  
+  // Remove non-alphanumeric characters and convert to lowercase
+    str1 = str1.replace(/\W/g, '').toLowerCase();
+    str2 = str2.replace(/\W/g, '').toLowerCase();
 
+    // If lengths don't match, they can't be anagrams
+    if (str1.length !== str2.length) return false;
+
+    // Sort and compare the characters
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
 
-module.exports = isAnagram;
+// Example usage:
+console.log(isAnagram("spar", "rasp")); // true
+console.log(isAnagram("listen", "silent")); // true
+console.log(isAnagram("hello", "world")); // false
+
